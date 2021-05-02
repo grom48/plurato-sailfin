1d5f7876c2874fcb52cdf1d0aef40ea238b955ab
grom48 <viktorbilokapicm@gmail.com>
2021-05-02 21:43:08 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".ham");
 const overlay = document.querySelector(".overlay")
 
 hamburger.addEventListener("click", () => {
