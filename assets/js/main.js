624bf6b2a6361dc180181dfaa10fff3c1a48f1b8
grom48 <viktorbilokapicm@gmail.com>
2021-05-07 16:33:06 +0200

    promjena

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
+$(".num").counterUp({delay:10,time: 500});
+
 const hamburger = document.querySelector(".ham");
-const overlay = document.querySelector(".overlay")
+const overlay = document.querySelector(".overlay");
 
 hamburger.addEventListener("click", () => {
     if (overlay.style.height === "50%") {
@@ -8,3 +10,16 @@ hamburger.addEventListener("click", () => {
         overlay.style.height = "50%"
     }
 })
+
+function imageFun() {
+    var Image_Id = document.querySelector(".yellow-dask");
+    if (Image_Id.scr.match("assets/img/yellow-dask.svg")) {
+        Image_Id.src = "assets/img/white-dask-svg";
+    } else {
+        Image_Id.src = "assets/img/yellow-dask.svg";
+    }
+}
+
+
+
+
