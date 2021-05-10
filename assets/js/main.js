055b78a4004b7be277820293483b4a9cb1f4d0f5
grom48 <viktorbilokapicm@gmail.com>
2021-05-10 19:21:36 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,5 +29,52 @@ red.addEventListener("click", () => {
 })
 
 
+function Clipboard_CopyTo(value) {
+    let tempInput = document.createElement("input");
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+  }
+  
+  document.querySelector('#Copy').onclick = function() {
+    Clipboard_CopyTo('Ul. Ruđera Boškovića 19, 21000, Split');
+  }
+
+  const accordion = document.querySelector('.accordion');
+  const items = accordion.querySelectorAll('.accordion__item');
+  
+  
+  items.forEach((item) => {
+    const title = item.querySelector('.accordion__title');
+    
+    title.addEventListener('click', (e) => {
+      const opened_item = accordion.querySelector('.is-open');
+      
+      // Toggle trenutni item
+      toggle_item(item); 
+      
+      // Zatvori prije otvorene iteme ako postoje
+      if (opened_item && opened_item !== item) {
+        toggle_item(opened_item);
+      }
+      
+      
+    });
+  });
+  
+  const toggle_item = (item) => {
+    const body = item.querySelector('.accordion__body');
+    const content = item.querySelector('.accordion__content');
+          
+    if (item.classList.contains('is-open')) {
+      body.removeAttribute('style');
+      item.classList.remove('is-open');
+    }else {
+      body.style.height = body.scrollHeight + 'px';
+      item.classList.add('is-open');
+    }
+  }
 
 
