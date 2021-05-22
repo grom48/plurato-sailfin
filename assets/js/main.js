c63c8c787e2d22a3ef2393ae21d230d21656de42
grom48 <viktorbilokapicm@gmail.com>
2021-05-22 16:12:17 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,33 +33,92 @@ red.addEventListener("click", () => {
     image.src = "assets/img/red-dask.png"
 })
 
-// !Kopiraj adresu
 
-function Clipboard_CopyTo(value) {
-    let tempInput = document.createElement("input");
-    tempInput.value = value;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
+
+// *Get the button:
+mybutton = document.getElementById("myBtn");
+
+// *When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+    }
+    
+    // *When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
   
-  document.querySelector('#Copy').onclick = function() {
-    Clipboard_CopyTo('Ul. Ruđera Boškovića 19, 21000, Split');
-  }
+  // *On scroll navbar
+  
+   $(function () { 
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 150) {
+        $('.nav .main-logo ').attr('src','assets/img/white-logo2.svg');
+      }
+      if ($(this).scrollTop() < 150) {
+        $('.nav .main-logo').attr('src','assets/img/logo.svg');
+      }
+    }) 
+    $(window).scroll(function () {
+      if($(window).scrollTop() > 150) {
+        $("nav").addClass('fixed-nav');
+      } else {
+        $("nav").removeClass('fixed-nav');
+      }
+    });
+  });  
+  
+  
+  // !Click components
+  $(function () {
+    $(window).on('hashchange', function () {
+      var tabContainers = $('.tabs > div'),
+      hash = window.location.hash != '' ? window.location.hash : '#first';
+      
+      console.log(hash)
+      
+      tabContainers.hide();
+      tabContainers.filter(hash).fadeIn();
+      $('.tabNavigation li a').removeClass('selected');
+      $('a[href="' + hash + '"]', '.tabNavigation').addClass('selected');
+    }).trigger('hashchange');
+  });
 
-  const button = document.querySelector(".copy-btn");
-  const tooltips = document.querySelector(".tooltips");
-
-  button.addEventListener("click", () => {
-    tooltips.classList.add("show");
-    setTimeout(function() {
-    tooltips.classList.remove("show");
-  }, 700)
-  })
-
-// !Accordion
-
+  
+  // !Kopiraj adresu
+  
+  function Clipboard_CopyTo(value) {
+      let tempInput = document.createElement("input");
+      tempInput.value = value;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+    }
+    
+    document.querySelector('#Copy').onclick = function() {
+      Clipboard_CopyTo('Ul. Ruđera Boškovića 19, 21000, Split');
+    }
+  
+    const button = document.querySelector(".copy-btn");
+    const tooltips = document.querySelector(".tooltips");
+  
+    button.addEventListener("click", () => {
+      tooltips.classList.add("show");
+      setTimeout(function() {
+      tooltips.classList.remove("show");
+    }, 700)
+    })
+  
+  // !Accordion
+  
   const accordion = document.querySelector('.accordion');
   const items = accordion.querySelectorAll('.accordion__item');
   
@@ -85,7 +144,7 @@ function Clipboard_CopyTo(value) {
   const toggle_item = (item) => {
     const body = item.querySelector('.accordion__body');
     const content = item.querySelector('.accordion__content');
-
+    
     if (item.classList.contains('is-open')) {
       body.removeAttribute('style');
       item.classList.remove('is-open');
@@ -94,62 +153,4 @@ function Clipboard_CopyTo(value) {
       item.classList.add('is-open');
     }
   }
-
-// *Get the button:
-mybutton = document.getElementById("myBtn");
-
-// *When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// *When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-// *On scroll navbar
-
-  $(function () { 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 150) {
-            $('.nav .main-logo ').attr('src','assets/img/white-logo2.svg');
-        }
-        if ($(this).scrollTop() < 150) {
-            $('.nav .main-logo').attr('src','assets/img/logo.svg');
-        }
-    }) 
-    $(window).scroll(function () {
-      if($(window).scrollTop() > 150) {
-          $("nav").addClass('fixed-nav');
-      } else {
-          $("nav").removeClass('fixed-nav');
-      }
-  });
-}); 
-
-
-// !Click components
-$(function () {
-    $(window).on('hashchange', function () {
-        var tabContainers = $('.tabs > div'),
-            hash = window.location.hash != '' ? window.location.hash : '#first';
-
-        console.log(hash)
-
-        tabContainers.hide();
-        tabContainers.filter(hash).fadeIn();
-        $('.tabNavigation li a').removeClass('selected');
-        $('a[href="' + hash + '"]', '.tabNavigation').addClass('selected');
-    }).trigger('hashchange');
-});
-
-
 
